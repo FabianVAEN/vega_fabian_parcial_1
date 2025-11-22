8ce9b841f843577ea005686643840a71ed2589b5
Fabian Vega <favegae@puce.edu.ec>
2025-11-22 11:32:07 -0500

    feature: colors added

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function generarFibonacci(n) {
-    let secuencia = [0, 1]; 
+    if (n === 1) return [1];
+    if (n === 2) return [1, 1];
+
+    let secuencia = [1, 1]; // Comenzar con F(1)=1, F(2)=1
 
     for (let i = 2; i < n; i++) {
         secuencia.push(secuencia[i - 1] + secuencia[i - 2]);
     }
+
     return secuencia;
 }
-
 
 const btnGenerar = document.getElementById("btnGenerar");
 const btnLimpiar = document.getElementById("btnLimpiar");
@@ -18,8 +21,9 @@ const mensajeError = document.getElementById("mensajeError")
 btnGenerar.addEventListener("click", function() {
     const cantidad = parseInt(entrada.value); 
 
+
     if (cantidad < 1 || cantidad > 50) {
-        mensajeError.textContent = "Por favor ingrese un el número entre 1 y 50.";
+        mensajeError.textContent = "Por favor ingrese un número entre 1 y 50.";
         return;
     }
 
@@ -29,7 +33,28 @@ btnGenerar.addEventListener("click", function() {
 
     output.innerHTML = "";
 
-    output.innerHTML = `<p>Secuencia de Fibonacci: ${secuencia.join(', ')}</p>`;
+    secuencia.forEach((valor, index) => {
+
+        const tarjeta = document.createElement("div");
+        tarjeta.classList.add("tarjeta");
+
+        let formula = "";
+        if (index === 0 || index === 1) {
+            formula = `F(${index + 1}) = ${valor}`; 
+            formula = `F(${index + 1}) = F(${index}) + F(${index - 1}) = ${secuencia[index - 1]} + ${secuencia[index - 2]} = ${valor}`;
+        }
+
+        const esPar = valor % 2 === 0 ? "par" : "impar";
+
+        tarjeta.classList.add(esPar);
+
+        tarjeta.innerHTML = `
+            <h3>Índice: F(${index + 1})</h3>
+            <p><strong>Valor:</strong> ${valor}</p>
+            <p><strong>Fórmula:</strong> ${formula}</p>
+        `;
+        output.appendChild(tarjeta);
+    });
 });
 
 btnLimpiar.addEventListener("click", function() {
